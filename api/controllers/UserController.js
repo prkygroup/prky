6ac9630041c42e072c9a9f30b821b480d415e6bc
Max Harris <mjharris@colorado.edu>
2015-09-20 01:38:39 -0600

    adds custom user create function to login user after signing in

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,14 @@
  */
 
 module.exports = {
-
+    create: function(req, res) {
+      User.create(req.body, function(err) {
+        if (err) {
+          res.send(500, {message: 'Unable to create user'});
+        }
+        else {
+          sails.controllers.auth.login(req, res);
+        }
+      });
+    }
 };
-
