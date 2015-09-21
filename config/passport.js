314c15aad35623eb04228daa33ca3c5b8e9387a2
Max Harris <mjharris@colorado.edu>
2015-09-20 19:47:42 -0600

    updates passport config to recognize passport-facebook

--- config/passport.js
+++ config/passport.js
@@ -1,12 +1,17 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
-
+    LocalStrategy = require('passport-local').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy;
 module.exports = {
     http: {
         customMiddleware: function(app) {
             console.log('Express middleware for passport');
             app.use( passport.initialize() );
             app.use( passport.session() );
+
+            app.use(function(req, res, next){
+                res.locals.user = req.session.user;
+                next();
+            });
         }
     }
 };
